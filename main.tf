45a70393d49765a0b2928e66c587cf3cbf70e7ef
Benjamin Mlynek <benjaminmlynek4@gmail.com>
2022-12-08 13:10:37 +0100

    Changed AMI & added TF Cloud backend config.

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  cloud {
+    organization = "organization-name"
+    workspaces {
+      name = "learn-tfc-aws"
+    }
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -10,10 +16,10 @@ terraform {
 }
 
 provider "aws" {
-  region  = "eu-west-1"
+  region = "eu-west-1"
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-08d70e59c07c61a3a"
+  ami           = "ami-07cd20392675b2b83"
   instance_type = "t2.micro"
 }
